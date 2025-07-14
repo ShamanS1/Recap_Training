20da09a36a3da9bf7f795c4ac4ff46ef9e015c67
Shaman <76400524+ShamanS1@users.noreply.github.com>
2025-07-14 10:30:32 +0530

    Create index.js

---
+++ Js/index.js
@@ -0,0 +1,22 @@
+function login()
+{
+    //show console
+    //complete the login here
+    //Call validatteForm method if response is Welcome then redirect to Welcome Page otherwise shows validation
+    // message in validationerror element
+}
+//complete this function
+function validateForm(username,password) {
+   
+  
+
+        // return 'Invalid credentials. Username cannot be empty';
+        // return 'Invalid credentials. Password should be at least 8 characters long.';
+     
+   
+
+    // If validations pass, the form will be submitted
+    // return "Welcome";
+}
+
+module.exports = validateForm;
